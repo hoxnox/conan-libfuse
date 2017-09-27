fe9b28a0847d73915efaae24a8b46e2d7735abb7
hoxnox <hoxnox@gmail.com>
2017-09-27 11:40:00 +0300

    ENH: Android support added

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from nxtools import NxConanFile
 from conans import AutoToolsBuildEnvironment,tools
 from os import chdir
-
+from glob import glob
 
 class LibFuseConan(NxConanFile):
     name = "libfuse"
@@ -25,14 +25,20 @@ class LibFuseConan(NxConanFile):
 
     def do_build(self):
         tools.untargz("libfuse-{v}.tar.gz".format(v=self.version), "{staging_dir}/src".format(staging_dir=self.staging_dir))
+        src_dir = "{staging_dir}/src/fuse-{v}".format(staging_dir=self.staging_dir, v=self.version)
+        if self.settings.os == "Android":
+            for file in sorted(glob("patch/*.patch")):
+                self.output.info("Applying patch '{file}'".format(file=file))
+                tools.patch(base_path=src_dir, patch_file=file, strip=0)
+
         shared_definition = "--enable-static", "--disable-shared"
         if self.options["libfuse"].shared:
             shared_definition = "--enable-shared", "--disable-static"
         chdir("{staging_dir}/src/fuse-{v}".format(staging_dir=self.staging_dir, v=self.version))
         env_build = AutoToolsBuildEnvironment(self)
         env_build.configure(args=["--prefix={staging_dir}".format(staging_dir=self.staging_dir),
-            shared_definition[0], shared_definition[1], "--disable-util", "--disable-example"])
-        env_build.make(['install'])
+            shared_definition[0], shared_definition[1], "--disable-util", "--disable-example", "--verbose"])
+        env_build.make(['V=1', 'install'])
 
     def do_package_info(self):
         self.cpp_info.libs = ["fuse"]
